b4225bf793aafb5d8296750f2d6ba9380b07f149
jaydeeptalsania <jaydeept007@gmail.com>
2023-05-31 16:22:14 +0530

    Rest operators and spread operators

--- javascript_for_node.js
+++ javascript_for_node.js
@@ -18,3 +18,29 @@ console.log(doubleAge);
  })
 
  console.log(newPrice);
+
+ //======================= Spread opretors =======================================
+
+  const arr1 = [1,2,3,4];
+  const arr2 = [5,6,7,8];
+
+  const arr3 = [...arr1,...arr2];   // combine two arrays with spread operators
+  console.log(typeof arr3);
+
+  //======================== rest operators =======================================
+
+  const demoFunc = (name, age, ...args) =>{  // it will take first and second parameter and creates an array of the remaining parameters
+        let sum = 0;
+        let i;
+        for(i = 0 ; i < args.length; i++){
+           sum = sum + args[i];
+        }
+        console.log(name);
+        console.log(age);
+        console.log(sum);
+  }
+
+  demoFunc("Jayd",33,10,20,30); // Here the function will consider "Jayd" as "name" & "33" as "age" , & for the rest of the values it will store in args(array)
+
+
+  
