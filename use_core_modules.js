22de569c69d25c0caa06f203b4985ad692df2a7d
jaydeeptalsania <jaydeept007@gmail.com>
2023-05-31 17:59:00 +0530

    using fs module to write and read the file data in sync mode

--- use_core_modules.js
+++ use_core_modules.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
    res.writeHead(200,{'Content-Type': 'text/javascript','demo':'test'});
@@ -8,3 +9,9 @@ const server = http.createServer((req,res)=>{
 server.listen('3001',()=>{
    console.log("The app is running on port 3001");
 });
+
+/*const data = "Hello this is a demo file";
+fs.writeFileSync('demo.txt',data);*/
+
+const fileData = fs.readFileSync('demo.txt','utf-8');
+console.log(fileData);
