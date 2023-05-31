d5d328ddb8d896f393a366ad314bb0f36a8a522d
jaydeeptalsania <jaydeept007@gmail.com>
2023-05-31 09:57:43 +0530

    import & export function inside an object

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 const person = require('./module_export_example');
-console.log(person.age , person.city , person.name);
+console.log(person.age , person.city , person.name , person.intro());
--- module_export_example.js
+++ module_export_example.js
@@ -1,7 +1,10 @@
 const person = {
     name:"Jaydeep",
     age:33,
-    city:"Gandhinagar"
+    city:"Gandhinagar",
+    intro:()=>{
+        return `My name is ${person.name} & my age is:${person.age}` // here {this.name} or {this.age} will not work because of arrow function
+    }
     
 }
 
