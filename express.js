1902454124be7b98b7e15365171e2104c2d0777d
jaydeeptalsania <jaydeept007@gmail.com>
2023-06-05 10:15:42 +0530

    more routing using express (send query parameters & send HTML as response)

--- express.js
+++ express.js
@@ -6,6 +6,22 @@ app.get('/',(req,res)=>{
     res.send('Basic express routing');
 });
 
+app.get('/user',(req,res)=>{
+    console.log(req.query.name , req.query.city); // send query parameters with url , for ex:- localhost::3002?name=jay&city=gandhinagar
+    res.send({
+       name:req.query.name,
+       city:req.query.city
+    });
+});
+
+app.get('/help',(req,res)=>{     // send HTML as response
+   res.send(
+    `<h1>Hello , How can I help you?</h1>
+     <p>you can visit our FAQs page</p>
+    `
+   )
+});
+
 app.listen(port,()=>{
     console.log(`Running on port :- ${port}`);
 });
