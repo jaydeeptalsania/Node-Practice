276fcc85ccb45d78f6ae80f3fdcc26840b4e2a5a
jaydeeptalsania <100285762+jaydeeptalsania@users.noreply.github.com>
2025-05-20 11:22:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-   res.end("Hello from new server");
+   res.end("Hello from new server 123");
 });
 
 server.listen(3001);
